5dbac7556402042fd34f0b5d89c613bd86424661
Jacob Robinson <jrobinsondev@gmail.com>
2018-04-24 13:50:43 -0400

    updated package-lock.json and semicolon

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -6,7 +6,7 @@
 // for more of what you can do here.
 module.exports = function (app) {
   const db = app.get('knexClient');
-  const tableName = 'users'
+  const tableName = 'users';
   db.schema.hasTable(tableName).then(exists => {
     if(!exists) {
       db.schema.createTable(tableName, table => {
