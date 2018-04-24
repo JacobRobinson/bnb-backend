e5f8ef96c2e49460abf21aec989d6db952b3c609
Jacob Robinson <jrobinsondev@gmail.com>
2018-04-24 13:48:33 -0400

    initialized services & models. next: foreign keys & book date range/interval

---
+++ src/models/bookings.model.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+
+// bookings-model.js - A KnexJS
+// 
+// See http://knexjs.org/
+// for more of what you can do here.
+module.exports = function (app) {
+  const db = app.get('knexClient');
+  const tableName = 'bookings';
+  db.schema.hasTable(tableName).then(exists => {
+    if(!exists) {
+      db.schema.createTable(tableName, table => {
+        table.increments('id');
+        //todo: add foreign keys & interval range
+      })
+        .then(() => console.log(`Created ${tableName} table`))
+        .catch(e => console.error(`Error creating ${tableName} table`, e));
+    }
+  });
+  
+
+  return db;
+};
---
+++ src/models/properties.model.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+
+// properties-model.js - A KnexJS
+// 
+// See http://knexjs.org/
+// for more of what you can do here.
+module.exports = function (app) {
+  const db = app.get('knexClient');
+  const tableName = 'properties';
+  db.schema.hasTable(tableName).then(exists => {
+    if(!exists) {
+      db.schema.createTable(tableName, table => {
+        table.increments('id').primary();
+        // table.foreign('owner');  //add column name, reference, FK
+        table.string('name');
+        table.string('address');    //update to multiple strings
+        table.string('type');       //restrict to vals
+        table.string('image_url');
+        table.integer('capacity');
+        table.integer('beds');
+        table.float('price');       //nightly
+      })
+        .then(() => console.log(`Created ${tableName} table`))
+        .catch(e => console.error(`Error creating ${tableName} table`, e));
+    }
+  });
+  
+
+  return db;
+};
